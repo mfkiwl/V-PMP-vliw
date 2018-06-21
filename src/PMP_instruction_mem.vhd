c746ee877fa9c1db140d9ecabbf075fc07c9c5f5
Marco Spaziani Brunella <marco.spaziani.brunella@uniroma2.it>
2018-06-21 11:16:06 +0200

    Modified instruction memory

--- src/PMP_instruction_mem.vhd
+++ src/PMP_instruction_mem.vhd
@@ -7,41 +7,44 @@ use std.textio.all;
 use ieee.std_logic_textio.all;
 
 entity i_mem is
-	  generic (init_file: string:="./i_mem.bin");
-      port ( 
-             --AXI/mem interface
-			 axi_clock : in std_logic;
-			 we: in std_logic;
-             axi_addr : in std_logic_vector(10 downto 0); 
-             axi_data_in : in std_logic_vector(31 downto 0);
-             axi_data_out : out std_logic_vector(31 downto 0);
-             
+
+  generic (init_file: string:="./i_mem.bin");
+
+  port ( 
+       --AXI/mem interface
+         axi_clock : in std_logic;
+         we: in std_logic;
+         axi_addr : in std_logic_vector(10 downto 0); 
+         axi_data_in : in std_logic_vector(31 downto 0);
+         axi_data_out : out std_logic_vector(31 downto 0);
+
              -- AXIS interface
-             clock : in std_logic;
-             reset : in std_logic;
- 			 addr  : in std_logic_vector(7 downto 0);
- 			 data_out : out std_logic_vector(255 downto 0)
-		);
+         clock : in std_logic;
+         reset : in std_logic;
+         addr  : in std_logic_vector(7 downto 0);
+         data_out : out std_logic_vector(511 downto 0)
+       );
+
 end i_mem;
 
 architecture behavioral of i_mem is
 
-type mem_type_128_256 is array (0 to 255) of std_logic_vector(255 downto 0) ;
+  type mem_type is array (0 to 255) of std_logic_vector(511 downto 0) ;
 
-impure function InitRamFromFile (RamFileName : in string) return mem_type_128_256 is
+  impure function InitRamFromFile (RamFileName : in string) return mem_type is
     FILE ramfile : text is in RamFileName;
     variable RamFileLine : line;
-    variable ram : mem_type_128_256;
-        begin
-            for i in mem_type_128_256'range loop
-                readline(ramfile, RamFileLine);
-                hread(RamFileLine, ram(i));
-            end loop;
+    variable ram : mem_type;
+  begin
+    for i in mem_type'range loop
+      readline(ramfile, RamFileLine);
+      hread(RamFileLine, ram(i));
+    end loop;
     return ram;
-end function;
+  end function;
 
-signal mem1: mem_type_128_256:=InitRamFromFile(init_file);
-signal word_data_out: std_logic_vector(255 downto 0);
+  signal mem1: mem_type:=InitRamFromFile(init_file);
+  signal word_data_out: std_logic_vector(511 downto 0);
 
 begin
 
@@ -70,26 +73,35 @@ begin
 --end process;
 
 
-axi_data_out<= word_data_out(31  downto   0) when axi_addr(2 downto 0)="000" else
-               word_data_out(63  downto  32) when axi_addr(2 downto 0)="001" else
-               word_data_out(95  downto  64) when axi_addr(2 downto 0)="010" else
-               word_data_out(127 downto  96) when axi_addr(2 downto 0)="011" else
-               word_data_out(159 downto 128) when axi_addr(2 downto 0)="100" else
-               word_data_out(191 downto 160) when axi_addr(2 downto 0)="101" else
-               word_data_out(223 downto 192) when axi_addr(2 downto 0)="110" else
-               word_data_out(255 downto 224);
+  --axi_data_out<= word_data_out(31  downto   0) when axi_addr(2 downto 0)="000" else
+  --               word_data_out(63  downto  32) when axi_addr(2 downto 0)="001" else
+  --              word_data_out(95  downto  64) when axi_addr(2 downto 0)="010" else
+  --               word_data_out(127 downto  96) when axi_addr(2 downto 0)="011" else
+  --               word_data_out(159 downto 128) when axi_addr(2 downto 0)="100" else
+  --               word_data_out(191 downto 160) when axi_addr(2 downto 0)="101" else
+  --               word_data_out(223 downto 192) when axi_addr(2 downto 0)="110" else
+  --               word_data_out(255 downto 224);
 
 
-process(clock)
-    begin
-	if rising_edge(clock) then
-	   if (reset = '1') then
-	        data_out <= (others => '0');
-	   else
-			data_out <= mem1(conv_integer(addr(7 downto 0)));
-	   end if;
+  process(clock)
+  
+  begin
+  
+    if rising_edge(clock) then
+  
+      if (reset = '1') then
+  
+        data_out <= (others => '0');
+  
+      else
+  
+        data_out <= mem1(conv_integer(addr(7 downto 0)));
+  
+      end if;
+  
     end if;			
-end process;
+  
+  end process;
 
 
 end behavioral;
