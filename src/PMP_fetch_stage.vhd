99cb7760d08aaa55b11ab4ee1db914aa6797c56f
Marco Spaziani Brunella <marco.spaziani.brunella@uniroma2.it>
2018-06-21 21:49:13 +0200

    TODO: fix registers on fetch

--- src/PMP_fetch_stage.vhd
+++ src/PMP_fetch_stage.vhd
@@ -15,7 +15,6 @@ entity fetch_stage is
          stop       : in std_logic; -- '1' when STOP syllable is decoded
          branch     : in std_logic; -- '1' when flush pipeline because branch
 
-
          syllable_0 : out std_logic_vector(63 downto 0); -- syllable 0
          syllable_1 : out std_logic_vector(63 downto 0); -- syllable 1
          syllable_2 : out std_logic_vector(63 downto 0); -- syllable 2
@@ -27,32 +26,14 @@ entity fetch_stage is
 
          -- General purpose registers prefetch
 
-         gr_0_0     : out std_logic_vector (3 downto 0); -- address of first operand of syllable 0
-         gr_0_1	    : out std_logic_vector (3 downto 0); -- address of second operand of syllable 0
-
-         gr_1_0     : out std_logic_vector (3 downto 0);
-         gr_1_1	    : out std_logic_vector (3 downto 0);
-
-         gr_2_0     : out std_logic_vector (3 downto 0);
-         gr_2_1	    : out std_logic_vector (3 downto 0);
-
-         gr_3_0     : out std_logic_vector (3 downto 0);
-         gr_3_1	    : out std_logic_vector (3 downto 0);
-
-         gr_3_0     : out std_logic_vector (4 downto 0); 
-         gr_3_1	    : out std_logic_vector (4 downto 0); 
-
-         gr_5_0     : out std_logic_vector (3 downto 0);
-         gr_5_1	    : out std_logic_vector (3 downto 0);
-
-         gr_6_0     : out std_logic_vector (3 downto 0);
-         gr_6_1	    : out std_logic_vector (3 downto 0);
-
-         gr_7_0     : out std_logic_vector (3 downto 0);
-         gr_7_1	    : out std_logic_vector (3 downto 0);
-
-         -- branch register prefetch
-         br_0       : out std_logic_vector (4 downto 0); -- address of branch register for syllable 0
+         gr_src_0     : out std_logic_vector (3 downto 0); -- address of first operand of syllable 0
+         gr_src_1     : out std_logic_vector (3 downto 0);
+         gr_src_2     : out std_logic_vector (3 downto 0);
+         gr_src_3     : out std_logic_vector (3 downto 0); 
+         gr_src_4     : out std_logic_vector (3 downto 0);
+         gr_src_5     : out std_logic_vector (3 downto 0);
+         gr_src_6     : out std_logic_vector (3 downto 0);
+         gr_src_7     : out std_logic_vector (3 downto 0);
 
          cycles     : out std_logic_vector(31 downto 0); -- number of clock cycles the execution took
          pc_inc     : out std_logic                      -- increment Program Counter
@@ -69,11 +50,34 @@ architecture behavioural of fetch_stage is
   signal stop_i      : std_logic := '0';
   signal out_valid_i : std_logic := '0';
 
+  signal  syllable_0_s : std_logic_vector(63 downto 0) := (others => '0');  
+  signal  syllable_1_s : std_logic_vector(63 downto 0) := (others => '0');
+  signal  syllable_2_s : std_logic_vector(63 downto 0) := (others => '0');
+  signal  syllable_3_s : std_logic_vector(63 downto 0) := (others => '0');
+  signal  syllable_4_s : std_logic_vector(63 downto 0) := (others => '0');
+  signal  syllable_5_s : std_logic_vector(63 downto 0) := (others => '0');
+  signal  syllable_6_s : std_logic_vector(63 downto 0) := (others => '0');
+  signal  syllable_7_s : std_logic_vector(63 downto 0) := (others => '0');
+
 begin	
 
+  -- chop instruction into syllables
+
+  syllable_0_s <= instr(63 downto 0);
+  syllable_1_s <= instr(127 downto 64);
+  syllable_2_s <= instr(191 downto 128);
+  syllable_3_s <= instr(255 downto 192);
+  syllable_4_s <= instr(319 downto 256);      
+  syllable_5_s <= instr(383 downto 320);
+  syllable_6_s <= instr(447 downto 384);
+  syllable_7_s <= instr(511 downto 448);
+
+
   cycles <= cycles_i;
+
   -- Counts running cycles
   cycle_counter : process(clk, reset)
+
   begin 
 
     if (reset = '1') then
@@ -84,11 +88,11 @@ begin
       if (running_s = '1' and stop_i = '0') then
 
         cycles_i <= cycles_i + 1;
-      
+
       else
-      
+
         cycles_i <= cycles_i;
-      
+
       end if;
 
     end if;
@@ -97,11 +101,11 @@ begin
 
   -- Output
   fetch_out : process(clk) is
-  
+
   begin
 
     if rising_edge(clk) then
-  
+
       -- Default case
 
       syllable_0 <= (others => '0');
@@ -113,127 +117,47 @@ begin
       syllable_6 <= (others => '0');
       syllable_7 <= (others => '0');
 
-      gr_0_0 <= (others => '0');                    
-      gr_0_1 <= (others => '0');                    
+      gr_src_0 <= (others => '0'); 
+      gr_src_1 <= (others => '0'); 
+      gr_src_2 <= (others => '0'); 
+      gr_src_3 <= (others => '0'); 
+      gr_src_4 <= (others => '0'); 
+      gr_src_5 <= (others => '0'); 
+      gr_src_6 <= (others => '0'); 
+      gr_src_7 <= (others => '0'); 
 
-      gr_1_0 <= (others => '0');                     
-      gr_1_1 <= (others => '0');                      
-
-      gr_2_0 <= (others => '0');                     
-      gr_2_1 <= (others => '0');                     
-
-      gr_3_0 <= (others => '0');
-      gr_3_1 <= (others => '0'); 
-
-
-      gr_4_0 <= (others => '0');
-      gr_4_1 <= (others => '0'); 
-
-      gr_5_0 <= (others => '0');
-      gr_5_1 <= (others => '0'); 
-
-      gr_6_0 <= (others => '0');
-      gr_6_1 <= (others => '0'); 
-
-      gr_7_0 <= (others => '0');
-      gr_7_1 <= (others => '0');    
-
-      br_0  <= (others => '0');
       pc_inc <= '0';
-
       running_s <= '0';
 
       if (start = '1' and reset = '0') then    
-
+      
+        -- control signals to program counter
         pc_inc <= '1';        
         running_s <= '1';
+      
+        -- outputting the syllables
+        syllable_0 <= syllable_0_s; 
+        syllable_1 <= syllable_1_s; 
+        syllable_2 <= syllable_2_s; 
+        syllable_3 <= syllable_3_s; 
+        syllable_4 <= syllable_4_s;      
+        syllable_5 <= syllable_5_s; 
+        syllable_6 <= syllable_6_s; 
+        syllable_7 <= syllable_7_s; 
 
-        syllable_0 <= instr(63 downto 0);
-        syllable_1 <= instr(127 downto 64);
-        syllable_2 <= instr(191 downto 128);
-        syllable_3 <= instr(255 downto 192);
-        syllable_4 <= instr(319 downto 256);      
-        syllable_5 <= instr(383 downto 320);
-        syllable_6 <= instr(447 downto 384;
-        syllable_7 <= instr(511 downto 448);
-
--- TODO
-        -- Syllable 0 prefetch and handling  
-        if (std_match(instr(31 downto 26), "0-----")) or (std_match(instr(31 downto 26), "10----")) then
-
-        gr_0_0 <= instr (25 downto 21); -- operand 0
-        gr_0_1 <= instr (20 downto 16); -- operand 1
-
-      elsif (std_match(instr(31 downto 26), J_TYPE) ) then
-
-        br_0 <= instr (25 downto 21);
-
+        -- source registers prefetch 
+        gr_src_0 <= syllable_0_s(15 downto 12);
+        gr_src_1 <= syllable_0_s(15 downto 12);
+        gr_src_2 <= syllable_0_s(15 downto 12);
+        gr_src_3 <= syllable_0_s(15 downto 12);
+        gr_src_4 <= syllable_0_s(15 downto 12);
+        gr_src_5 <= syllable_0_s(15 downto 12);
+        gr_src_6 <= syllable_0_s(15 downto 12);
+        gr_src_7 <= syllable_0_s(15 downto 12);
 
       end if;
-      -- End syllable 0 handling       
-
-      -- Syllable 1 prefetch and handling  
-
-
-      gr_1_0 <= instr (57 downto 53);
-      gr_1_1 <= instr (52 downto 48);
-
-
-
-    -- End syllable 1 handling       
-
-    -- Syllable 2 prefetch and handling  
-
-
-      gr_2_0 <= instr(89 downto 85);
-      gr_2_1 <= instr(84 downto 80);
-
-  -- End syllable 2 handling       
-
-  -- Syllable 3 prefetch and handling  
-
-
-      gr_3_0 <= instr(121 downto 117);
-      gr_3_1 <= instr(116 downto 112);
-
--- End syllable 3 handling       
-
--- Syllable 4 prefetch and handling  
-
-      gr_4_0 <= instr(153 downto 149);
-      gr_4_1 <= instr(148 downto 144);
-
-        -- End syllable 4 handling       
-
-        -- Syllable 5 prefetch and handling  
-
-
-      gr_5_0 <= instr(185 downto 181);
-      gr_5_1 <= instr(180 downto 176);
-
-      -- End syllable 5 handling       
-
-      -- Syllable 6 prefetch and handling  
-
-
-      gr_6_0 <= instr(217 downto 213);
-      gr_6_1 <= instr(212 downto 208);
-
-    -- End syllable 6 handling       
-
-    -- Syllable 7 prefetch and handling  
-
-
-      gr_7_0 <= instr(249 downto 245);
-      gr_7_1 <= instr(244 downto 240);
-
-   -- End syllable 7 handling       
     end if;
-  end if;
-end process fetch_out;
-
--- Controls syllable fetch stage
-
+  end process fetch_out;
 
 end architecture behavioural;
 
--- tb/fetch_tb.vhd
+++ tb/fetch_tb.vhd
@@ -3,31 +3,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity fetch_tb is
 
-port (
-	     syllable_0 : out std_logic_vector(31 downto 0); -- syllable 0
-	     syllable_1 : out std_logic_vector(31 downto 0); -- syllable 1
-	     syllable_2 : out std_logic_vector(31 downto 0); -- syllable 2
-	     syllable_3 : out std_logic_vector(31 downto 0); -- syllable 3
-	     syllable_4 : out std_logic_vector(31 downto 0); -- syllable 4 
-         syllable_5 : out std_logic_vector(31 downto 0); -- syllable 5 
-         syllable_6 : out std_logic_vector(31 downto 0); -- syllable 6 
-         syllable_7 : out std_logic_vector(31 downto 0); -- syllable 7 
+  port (
+         syllable_0 : out std_logic_vector(63 downto 0); -- syllable 0
+         syllable_1 : out std_logic_vector(63 downto 0); -- syllable 1
+         syllable_2 : out std_logic_vector(63 downto 0); -- syllable 2
+         syllable_3 : out std_logic_vector(63 downto 0); -- syllable 3
+         syllable_4 : out std_logic_vector(63 downto 0); -- syllable 4 
+         syllable_5 : out std_logic_vector(63 downto 0); -- syllable 5 
+         syllable_6 : out std_logic_vector(63 downto 0); -- syllable 6 
+         syllable_7 : out std_logic_vector(63 downto 0); -- syllable 7 
 
-         br_0       : out std_logic_vector (4 downto 0);
-         br_1       : out std_logic_vector (4 downto 0);
-         br_2       : out std_logic_vector (4 downto 0);
-         br_3       : out std_logic_vector (4 downto 0);
-         br_4       : out std_logic_vector (4 downto 0);
-         br_5       : out std_logic_vector (4 downto 0);
-         br_6       : out std_logic_vector (4 downto 0);
-         br_7       : out std_logic_vector (4 downto 0);
+         cycles     : out std_logic_vector(31 downto 0); -- number of clock cycles the execution took
+         pc_inc     : out std_logic
 
-	     cycles     : out std_logic_vector(31 downto 0); -- number of clock cycles the execution took
-         pc_inc    : out std_logic;  
-	     fetch_ready: out std_logic;                     -- 1 when ready to fetch another instruction
-	     fetch_done : out std_logic);                    -- '1' when syllables are valid
+);   
 
-  end fetch_tb;
+end fetch_tb;
 
 architecture Behavioral of fetch_tb is
 
@@ -37,242 +28,193 @@ architecture Behavioral of fetch_tb is
   signal start      : std_logic := '0';
   signal stop       : std_logic := '1';
   signal PC         : std_logic_vector (7 downto 0) := (others => '0');
-  signal instr      : std_logic_vector (255 downto 0);
-  signal stall_s    : std_logic := '0';
+  signal instr      : std_logic_vector (511 downto 0);
   signal pc_inc_s   : std_logic := '0';
   signal branch_add : std_logic_vector (7 downto 0) := (others => '0');
-
-signal  add_0_0  : std_logic_vector(4 downto 0);
-signal  add_0_1  : std_logic_vector(4 downto 0);
-signal  add_0_d  : std_logic_vector(4 downto 0);
-signal  w_e_0    : std_logic;
-signal  cont_0_0 : std_logic_vector(31 downto 0);
-signal  cont_0_1 : std_logic_vector(31 downto 0);
-signal  cont_0_d : std_logic_vector(31 downto 0);
-
-signal  add_1_0  : std_logic_vector(4 downto 0);
-signal  add_1_1  : std_logic_vector(4 downto 0);
-signal  add_1_d  : std_logic_vector(4 downto 0);
-signal  w_e_1    : std_logic;
-signal  cont_1_0 : std_logic_vector(31 downto 0);
-signal  cont_1_1 : std_logic_vector(31 downto 0);
-signal  cont_1_d : std_logic_vector(31 downto 0);
-
-signal  add_2_0  : std_logic_vector(4 downto 0);
-signal  add_2_1  : std_logic_vector(4 downto 0);
-signal  add_2_d  : std_logic_vector(4 downto 0);
-signal  w_e_2    : std_logic;
-signal  cont_2_0 : std_logic_vector(31 downto 0);
-signal  cont_2_1 : std_logic_vector(31 downto 0);
-signal  cont_2_d : std_logic_vector(31 downto 0);
-
-signal  add_3_0  : std_logic_vector(4 downto 0);
-signal  add_3_1  : std_logic_vector(4 downto 0);
-signal  add_3_d  : std_logic_vector(4 downto 0);
-signal  w_e_3    : std_logic;
-signal  cont_3_0 : std_logic_vector(31 downto 0);
-signal  cont_3_1 : std_logic_vector(31 downto 0);
-signal  cont_3_d : std_logic_vector(31 downto 0);
-
-signal  add_4_0  : std_logic_vector(4 downto 0);
-signal  add_4_1  : std_logic_vector(4 downto 0);
-signal  add_4_d  : std_logic_vector(4 downto 0);
-signal  w_e_4    : std_logic;
-signal  cont_4_0 : std_logic_vector(31 downto 0);
-signal  cont_4_1 : std_logic_vector(31 downto 0);
-signal  cont_4_d : std_logic_vector(31 downto 0);
-
-signal  add_5_0  : std_logic_vector(4 downto 0);
-signal  add_5_1  : std_logic_vector(4 downto 0);
-signal  add_5_d  : std_logic_vector(4 downto 0);
-signal  w_e_5    : std_logic;
-signal  cont_5_0 : std_logic_vector(31 downto 0);
-signal  cont_5_1 : std_logic_vector(31 downto 0);
-signal  cont_5_d : std_logic_vector(31 downto 0);
-
-signal  add_6_0  : std_logic_vector(4 downto 0);
-signal  add_6_1  : std_logic_vector(4 downto 0);
-signal  add_6_d  : std_logic_vector(4 downto 0);
-signal  w_e_6    : std_logic;
-signal  cont_6_0 : std_logic_vector(31 downto 0);
-signal  cont_6_1 : std_logic_vector(31 downto 0);
-signal  cont_6_d : std_logic_vector(31 downto 0);
   
-signal  add_7_0  : std_logic_vector(4 downto 0);
-signal  add_7_1  : std_logic_vector(4 downto 0);
-signal  add_7_d  : std_logic_vector(4 downto 0);
-signal  w_e_7    : std_logic;
-signal  cont_7_0 : std_logic_vector(31 downto 0);
-signal  cont_7_1 : std_logic_vector(31 downto 0);
-signal  cont_7_d : std_logic_vector(31 downto 0);
+  -- syllable 0
+  signal add_src_0  :  std_logic_vector(3 downto 0); -- address of syllable 0 source operand  
+  signal add_dst_0  :  std_logic_vector(3 downto 0); -- address of syllable 0 result from execution stage
+  signal w_e_0      :  std_logic;
+  signal cont_src_0 :  std_logic_vector(63 downto 0); -- syllable 0 source operand
+  signal cont_dst_0 :  std_logic_vector(63 downto 0);  -- syllable 0 result from execution stage
 
-signal instr_valid : std_logic;
+  -- syllable 1 -
+  signal add_src_1  :  std_logic_vector(3 downto 0); -- address of syllable 0 source operand  
+  signal add_dst_1  :  std_logic_vector(3 downto 0); -- address of syllable 0 result from execution stage
+  signal w_e_1      :  std_logic;
+  signal cont_src_1 :  std_logic_vector(63 downto 0); -- syllable 1 source operand
+  signal cont_dst_1 :  std_logic_vector(63 downto 0);  -- syllable 1 result from execution stage
+
+  -- syllable 2 -
+  signal add_src_2  :  std_logic_vector(3 downto 0); -- address of syllable 1 source operand  
+  signal add_dst_2  :  std_logic_vector(3 downto 0); -- address of syllable 1 result from execution stage
+  signal w_e_2      :  std_logic;
+  signal cont_src_2 :  std_logic_vector(63 downto 0); -- syllable 2 source operand
+  signal cont_dst_2 :  std_logic_vector(63 downto 0);  -- syllable 2 result from execution stage
+
+  -- syllable 3 -
+  signal add_src_3  :  std_logic_vector(3 downto 0); -- address of syllable 2 source operand  
+  signal add_dst_3  :  std_logic_vector(3 downto 0); -- address of syllable 2 result from execution stage
+  signal w_e_3      :  std_logic;
+  signal cont_src_3 :  std_logic_vector(63 downto 0); -- syllable 3 source operand
+  signal cont_dst_3 :  std_logic_vector(63 downto 0);  -- syllable 3 result from execution stage
+
+  -- syllable 4 -
+  signal add_src_4  :  std_logic_vector(3 downto 0); -- address of syllable 4 source operand  
+  signal add_dst_4  :  std_logic_vector(3 downto 0); -- address of syllable 4 result from execution stage
+  signal w_e_4      :  std_logic;
+  signal cont_src_4 :  std_logic_vector(63 downto 0); -- syllable 4 source operand
+  signal cont_dst_4 :  std_logic_vector(63 downto 0);  -- syllable 4 result from execution stage
+
+  -- syllable 5 -
+  signal add_src_5  :  std_logic_vector(3 downto 0); -- address of syllable 5 source operand  
+  signal add_dst_5  :  std_logic_vector(3 downto 0); -- address of syllable 5 result from execution stage
+  signal w_e_5      :  std_logic;
+  signal cont_src_5 :  std_logic_vector(63 downto 0); -- syllable 5 source operand
+  signal cont_dst_5 :  std_logic_vector(63 downto 0);  -- syllable 5 result from execution stage
+
+  -- syllable 6 -
+  signal add_src_6  :  std_logic_vector(3 downto 0); -- address of syllable 6 source operand  
+  signal add_dst_6  :  std_logic_vector(3 downto 0); -- address of syllable 6 result from execution stage
+  signal w_e_6      :  std_logic;
+  signal cont_src_6 :  std_logic_vector(63 downto 0); -- syllable 6 source operand
+  signal cont_dst_6 :  std_logic_vector(63 downto 0);  -- syllable 6 result from execution stage
+
+  -- syllable 7 -
+  signal add_src_7  :  std_logic_vector(3 downto 0); -- address of syllable 7 source operand  
+  signal add_dst_7  :  std_logic_vector(3 downto 0); -- address of syllable 7 result from execution stage
+  signal w_e_7      :  std_logic;
+  signal cont_src_7 :  std_logic_vector(63 downto 0); -- syllable 7 source operand
+  signal cont_dst_7 :  std_logic_vector(63 downto 0);  -- syllable 7 result from execution stage
+
+
+  signal instr_valid : std_logic;
 
 begin
 
 
-	GPR_FILE: entity work.gr_regfile port map (
-											   clk,
-											   reset,
-											   add_0_0 ,
-											   add_0_1 ,
-											   add_0_d ,
-											   w_e_0   ,
-											   cont_0_0,
-											   cont_0_1,
-											   cont_0_d,
-											           											 
-											   add_1_0 ,
-											   add_1_1 ,
-											   add_1_d ,
-											   w_e_1   ,
-											   cont_1_0,
-											   cont_1_1,
-											   cont_1_d,
-											           
-											   add_2_0 ,
-											   add_2_1 ,
-											   add_2_d ,
-											   w_e_2   ,
-											   cont_2_0,
-											   cont_2_1,
-											   cont_2_d,
-											           
-											   add_3_0 ,
-											   add_3_1 ,
-											   add_3_d ,
-											   w_e_3   ,
-											   cont_3_0,
-											   cont_3_1,
-											   cont_3_d,
-											           
-											   add_4_0 ,
-											   add_4_1 ,
-											   add_4_d ,
-											   w_e_4   ,
-											   cont_4_0,
-											   cont_4_1,
-											   cont_4_d,
-											           
-											   add_5_0 ,
-											   add_5_1 ,
-											   add_5_d ,
-											   w_e_5   ,
-											   cont_5_0,
-											   cont_5_1,
-											   cont_5_d,
-											           
-											   add_6_0 ,
-											   add_6_1 ,
-											   add_6_d ,
-											   w_e_6   ,
-											   cont_6_0,
-											   cont_6_1,
-											   cont_6_d,
-											           
-											   add_7_0 ,
-											   add_7_1 ,
-											   add_7_d ,
-											   w_e_7   ,
-											   cont_7_0,
-											   cont_7_1,
-											   cont_7_d
-											   );
-	
-	
+  GPR_FILE: entity work.gr_regfile port map (
+
+  clk,
+  reset, 
+
+  add_src_0,  
+  add_dst_0,   
+  w_e_0,      
+  cont_src_0, 
+  cont_dst_0, 
+
+  add_src_1,  
+  add_dst_1,   
+  w_e_1,      
+  cont_src_1, 
+  cont_dst_1, 
+
+  add_src_2,  
+  add_dst_2,   
+  w_e_2,      
+  cont_src_2, 
+  cont_dst_2, 
+
+  add_src_3,  
+  add_dst_3,   
+  w_e_3,      
+  cont_src_3, 
+  cont_dst_3, 
+
+  add_src_4,  
+  add_dst_4,   
+  w_e_4,      
+  cont_src_4, 
+  cont_dst_4, 
+
+  add_src_5,  
+  add_dst_5,   
+  w_e_5,      
+  cont_src_5, 
+  cont_dst_5, 
+
+  add_src_6,  
+  add_dst_6,   
+  w_e_6,      
+  cont_src_6, 
+  cont_dst_6, 
+
+  add_src_7,  
+  add_dst_7,   
+  w_e_7,      
+  cont_src_7, 
+  cont_dst_7 
 
 
-  PROGRAM_COUNTER: entity work.pc port map (
-  											clk,
-  											pc_inc_s,
-  											reset,
-  											branch,
-  											branch_add,
-  											PC
-  											);
+);
 
-  FETCH_STAGE: entity work.fetch_stage port map (clk,        
-                                                       reset,     
-                                                       instr,
-                                                       
-                                                       start,     
-                                                       stop, 
-                                                       branch,     
-                                                      
-                                                       syllable_0, 
-                                                       syllable_1, 
-                                                       syllable_2 ,
-                                                       syllable_3 ,
-                                                       syllable_4 ,
-                                                       syllable_5 ,
-                                                       syllable_6 ,
-                                                       syllable_7 ,
-                                                                  
-                                                      
-                                                       add_0_0,     
-                                                       add_0_1,	    
-                                                    
-                                                       add_1_0,     
-                                                       add_1_1,	    
-                                                      
-                                                       add_2_0,     
-                                                       add_2_1,	    
-                                                       
-                                                       add_3_0,     
-                                                       add_3_1,	    
-                                                        
-                                                       add_4_0,     
-                                                       add_4_1,	    
-                                                       add_5_0,     
-                                                       add_5_1,	    
-                                                            
-                                                       add_6_0,     
-                                                       add_6_1,	    
-                                                            
-                                                       add_7_0,     
-                                                       add_7_1,	    
-                                                       
-                                                       br_0,       
-                                                                  
-                                                       cycles,     
-                                                       pc_inc_s                                                      
-                                                     );
+PROGRAM_COUNTER: entity work.pc port map (
+clk,
+pc_inc_s,
+reset,
+branch,
+branch_add,
+PC
+                        );
 
-I_MEM: entity work.i_mem port map (
-                                    clock => clk,
-                                    reset => reset,
-                                    addr => PC,
-                                    data_out => instr,
-                                    
-                                    axi_clock => '0',
-                                    we => '0',
-                                    axi_addr => (others => '0'),
-                                    axi_data_in => (others => '0')
-                                    );
-                                    
+                        FETCH_STAGE: entity work.fetch_stage port map (
+                        
+                        clk,        
+                        reset,     
+                        instr,
 
-clk <= not(clk) after 10ns;
-reset <= '0' after 15ns;
-start <= '1' after 13ns;
-stop <= '0' after 5ns;
---branch <= '1' after 290ns, '0' after 310ns;
-branch_add <= x"aa";
-pc_inc <= pc_inc_s;
+                        start,     
+                        stop, 
+                        branch,     
+
+                        syllable_0, 
+                        syllable_1, 
+                        syllable_2 ,
+                        syllable_3 ,
+                        syllable_4 ,
+                        syllable_5 ,
+                        syllable_6 ,
+                        syllable_7 ,
+
+                        add_src_0,     
+                        add_src_1,     
+                        add_src_2,     
+                        add_src_3,     
+                        add_src_4,     
+                        add_src_5,     
+                        add_src_6,     
+                        add_src_7,     
+
+                        cycles,     
+                        pc_inc_s                                                      
+                      );
+
+                      I_MEM: entity work.i_mem port map (
+                                                          clock => clk,
+                                                          reset => reset,
+                                                          addr => PC,
+                                                          data_out => instr,
+
+                                                          axi_clock => '0',
+                                                          we => '0',
+                                                          axi_addr => (others => '0'),
+                                                          axi_data_in => (others => '0')
+                                                        );
+
+
+                      clk <= not(clk) after 10ns;
+                      reset <= '0' after 15ns;
+                      start <= '1' after 13ns;
+                      stop <= '0' after 5ns;
+                      --branch <= '1' after 290ns, '0' after 310ns;
+                      branch_add <= x"aa";
+                      pc_inc <= pc_inc_s;
 
 
 
-
-
-
-
-
-
-
-
-         
-         
-         
 end architecture behavioral;
-         
-         
+
+
 
