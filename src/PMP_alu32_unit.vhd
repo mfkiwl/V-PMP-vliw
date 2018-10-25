c94b815953026acc8c168e4482c97f2a99afa1da
marcux95 <marco.spaziani.brunella@uniroma2.it>
2018-10-25 22:50:32 +0200

    added alu64 and alu 32

--- src/PMP_alu32_unit.vhd
+++ src/PMP_alu32_unit.vhd
@@ -41,6 +41,7 @@ begin
         result_gr <= (others => '0');
         gr_add_w <= (others => '0');
         w_e_gr <= '0';
+        result_gr(63 downto 32) <= (others => '0');
 
 
         if (alu64_select = '1') then -- START EXECUTING
@@ -55,151 +56,151 @@ begin
 
                 when ADDI_OPC =>
 
-                    result_gr <= operand_dst + (0x"00000000" & immediate);
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0)(31 downto 0) + (immediate);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when ADD_OPC =>
 
-                    result_gr <= operand_dst + operand_src;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) + operand_src(31 downto 0);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when SUBI_OPC =>
 
-                    result_gr <= operand_dst - (0x"00000000" & immediate);
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) - (immediate);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when SUB_OPC =>
 
-                    result_gr <= operand_dst - operand_src;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) - operand_src(31 downto 0);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when MULI_OPC =>
 
-                    result_gr <= std_logic_vector(signed(immediate)*signed(operand_dst));
+                    result_gr(31 downto 0) <= std_logic_vector(signed(immediate)*signed(operand_dst(31 downto 0)));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when MUL_OPC =>
 
-                    result_gr <= std_logic_vector(signed(operand_src)*signed(operand_dst));
+                    result_gr(31 downto 0) <= std_logic_vector(signed(operand_src(31 downto 0))*signed(operand_dst(31 downto 0)));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when DIVI_OPC =>
 
-                    result_gr <= std_logic_vector(to_signed(to_integer(signed(operand_dst) / signed(immediate)),64));
+                    result_gr(31 downto 0) <= std_logic_vector(to_signed(to_integer(signed(operand_dst(31 downto 0)) / signed(immediate)),64));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when DIVI_OPC =>
 
-                    result_gr <= std_logic_vector(to_signed(to_integer(signed(operand_dst) / signed(operand_src)),64));
+                    result_gr(31 downto 0) <= std_logic_vector(to_signed(to_integer(signed(operand_dst(31 downto 0)) / signed(operand_src(31 downto 0))),64));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when ORI_OPC =>
 
-                    result_gr <= operand_dst or  0x"00000000" & immediate;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) or immediate;
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when OR_OPC =>
 
-                    result_gr <= operand_dst or operand_src;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) or operand_src(31 downto 0);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when ANDI_OPC =>
 
-                    result_gr <= operand_dst and  0x"00000000" & immediate;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) and immediate;
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when AND_OPC =>
 
-                    result_gr <= operand_dst and operand_src;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) and operand_src(31 downto 0);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when LSHI_OPC =>
 
-                    result_gr <= shift_left(unsigned(operand_dst),unsigned(immediate));
+                    result_gr(31 downto 0) <= shift_left(unsigned(operand_dst(31 downto 0)),unsigned(immediate));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when LSH_OPC =>
 
-                    result_gr <= shift_left(unsigned(operand_dst),unsigned(operand_src));
+                    result_gr(31 downto 0) <= shift_left(unsigned(operand_dst(31 downto 0)),unsigned(operand_src(31 downto 0)));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when RSHI_OPC =>
 
-                    result_gr <= shift_right(unsigned(operand_dst),unsigned(immediate));
+                    result_gr(31 downto 0) <= shift_right(unsigned(operand_dst(31 downto 0)),unsigned(immediate));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when RSH_OPC =>
 
-                    result_gr <= shift_right(unsigned(operand_dst),unsigned(operand_src));
+                    result_gr(31 downto 0) <= shift_right(unsigned(operand_dst(31 downto 0)),unsigned(operand_src(31 downto 0)));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when NEG_OPC =>
 
-                    result_gr <= std_logic_vector(signed(operand_dst)*-1);
+                    result_gr(31 downto 0) <= std_logic_vector(signed(operand_dst(31 downto 0))*-1);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when MODI_OPC =>
 
-                    result_gr <= std_logic_vector(signed(operand_dst) mod signed(immediate));
+                    result_gr(31 downto 0) <= std_logic_vector(signed(operand_dst(31 downto 0)) mod signed(immediate));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when MOD_OPC =>
 
-                    result_gr <= std_logic_vector(signed(operand_dst) mod signed(operand_src)));
+                    result_gr(31 downto 0) <= std_logic_vector(signed(operand_dst(31 downto 0)) mod signed(operand_src(31 downto 0))));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when XORI_OPC =>
 
-                    result_gr <= operand_dst xor 0x"00000000" & immediate;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) xor immediate;
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when XOR_OPC =>
 
-                    result_gr <= operand_dst xor operand_src;
+                    result_gr(31 downto 0) <= operand_dst(31 downto 0) xor operand_src(31 downto 0);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when MOVI_OPC =>
 
-                    result_gr <= 0x"00000000" & immediate;
+                    result_gr(31 downto 0) <= immediate;
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when MOV_OPC =>
 
-                    result_gr <= operand_src;
+                    result_gr(31 downto 0) <= operand_src(31 downto 0);
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when ARSHI_OPC =>
 
-                    result_gr <= shift_right(signed(operand_dst),unsigned(immediate));
+                    result_gr(31 downto 0) <= shift_right(signed(operand_dst(31 downto 0)),unsigned(immediate));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
                 when ARSH_OPC =>
 
-                    result_gr <= shift_right(signed(operand_dst),unsigned(operand_src));
+                    result_gr(31 downto 0) <= shift_right(signed(operand_dst(31 downto 0)),unsigned(operand_src(31 downto 0)));
                     gr_add_wrt <= gr_add_dst;
                     w_e_gr <= '1';
 
--- src/PMP_exe_stage_ctrl.vhd
+++ src/PMP_exe_stage_ctrl.vhd
@@ -9,12 +9,13 @@ entity exe_stage_complete is
              reset                     : in std_logic;
 
              exe_operand_src           : in std_logic_vector(63 downto 0); -- SRC reg content
+             exe_operand_dst           : in std_logic_vector(63 downto 0); -- DST reg content
              exe_immediate             : in std_logic_vector(31 downto 0); -- immediate in the instruction
              exe_opc                   : in std_logic_vector(1 downto 0);  -- execution stage opc 00=alu64, 01=alu32, 10= mem, 11= branch
-             exe_dest_reg              : in std_logic_vector(3 downto 0);  -- exe stage destination register for writeback 
-             exe_offset                : in std_logic_vector(15 downto 0);
+             exe_dst_addr              : in std_logic_vector(3 downto 0);  -- exe stage destination register for writeback 
+             exe_offset                : in std_logic_vector(15 downto 0); -- offset inside instruction
 
-             exe_result                : out std_logic_vector(63 downto 0);  -- result from EXE stage for lane forwarding
+             exe_result                : out std_logic_vector(63 downto 0);  -- result from EXE stage for lane forwarding and writeback
              wb_reg_add                : out std_logic_vector(3 downto 0)   -- current register address in writeback from exe stage  -> for lane forwrding   
 
 
@@ -24,16 +25,16 @@ end exe_stage_complete;
 
 architecture Behavioral of exe_stage_complete is
 
---signal alu_gr_result_s : std_logic_vector(31 downto 0);
---signal alu_br_result_s : std_logic;
---signal alu_gr_add_s    : std_logic_vector(4 downto 0);
---signal alu_br_add_s    : std_logic_vector(4 downto 0);
---signal alu_gr_w_e_s    : std_logic;
---signal alu_br_w_e_s    : std_logic;
+    --signal alu_gr_result_s : std_logic_vector(31 downto 0);
+    --signal alu_br_result_s : std_logic;
+    --signal alu_gr_add_s    : std_logic_vector(4 downto 0);
+    --signal alu_br_add_s    : std_logic_vector(4 downto 0);
+    --signal alu_gr_w_e_s    : std_logic;
+    --signal alu_br_w_e_s    : std_logic;
 
---signal mem_gr_result_s : std_logic_vector(31 downto 0);
---signal mem_gr_add_s    : std_logic_vector(4 downto 0);
---signal mem_gr_w_e_s    : std_logic;
+    --signal mem_gr_result_s : std_logic_vector(31 downto 0);
+    --signal mem_gr_add_s    : std_logic_vector(4 downto 0);
+    --signal mem_gr_w_e_s    : std_logic;
 
     signal mem_select_s          : std_logic;
     signal alu_32_select_s       : std_logic;
@@ -46,53 +47,53 @@ begin
 
 
 
---ALU: entity work.alu_pmp port map (
---                                    alu_select_n_s,
+    --ALU: entity work.alu_pmp port map (
+    --                                    alu_select_n_s,
 
---                                    syllable,
---                                    alu_oper_0,
---                                    alu_oper_1,
---                                    alu_immediate_s,
---                                    alu_gr_dest_add,
---                                    alu_br_dest_add,
+    --                                    syllable,
+    --                                    alu_oper_0,
+    --                                    alu_oper_1,
+    --                                    alu_immediate_s,
+    --                                    alu_gr_dest_add,
+    --                                    alu_br_dest_add,
 
---                                    alu_gr_add_s,
---                                    alu_br_add_s,
---                                    alu_gr_w_e_s,
---                                    alu_br_w_e_s,
---                                    alu_gr_result_s,
---                                    alu_br_result_s
+    --                                    alu_gr_add_s,
+    --                                    alu_br_add_s,
+    --                                    alu_gr_w_e_s,
+    --                                    alu_br_w_e_s,
+    --                                    alu_gr_result_s,
+    --                                    alu_br_result_s
 
---                                    );
+    --                                    );
 
---MEM: entity work.mem_pmp port map (
---                                    mem_select_n_s,
+    --MEM: entity work.mem_pmp port map (
+    --                                    mem_select_n_s,
 
---                                    syllable,
---                                    mem_dest_reg,
---                                    mem_l_s,
---                                    mem_store_data,
---                                    mem_data_in,
---                                    mem_add_dec,
---                                    mem_add_wrt,
---                                    mem_data_out,
---                                    mem_wrt_amnt,
---                                    mem_w_e,
---                                    mem_gr_result_s,
---                                    mem_gr_add_s,
---                                    mem_gr_w_e_s
+    --                                    syllable,
+    --                                    mem_dest_reg,
+    --                                    mem_l_s,
+    --                                    mem_store_data,
+    --                                    mem_data_in,
+    --                                    mem_add_dec,
+    --                                    mem_add_wrt,
+    --                                    mem_data_out,
+    --                                    mem_wrt_amnt,
+    --                                    mem_w_e,
+    --                                    mem_gr_result_s,
+    --                                    mem_gr_add_s,
+    --                                    mem_gr_w_e_s
 
---                                   );
+    --                                   );
 
 
---CTRL: entity work.ctrl_pmp port map (
---                                    ctrl_select_n_s,
---                                    alu_br_cont,
---                                    syllable,
---                                    branch_add,
---                                    branch_valid
+    --CTRL: entity work.ctrl_pmp port map (
+    --                                    ctrl_select_n_s,
+    --                                    alu_br_cont,
+    --                                    syllable,
+    --                                    branch_add,
+    --                                    branch_valid
 
---                                    );  
+    --                                    );  
 
     alu_64_select_s <= '1' when exe_opc = "00" else
                        '0';
@@ -101,9 +102,9 @@ begin
                        '0';
 
     mem_select_s <= '1' when exe_opc = "10" else
-                       '0';
+                    '0';
 
     ctrl_select_s <= '1' when exe_opc = "11" else
-                       '0';
+                     '0';
 
 end Behavioral;
