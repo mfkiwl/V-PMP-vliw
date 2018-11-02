f7a37341d5f53cc6bbf69233aa7d9290aad44998
marcux95 <marco.spaziani.brunella@uniroma2.it>
2018-11-02 17:24:07 +0100

    Control created

--- src/PMP_ctrl.vhd
+++ src/PMP_ctrl.vhd
@@ -10,61 +10,263 @@ use work.common_pkg.all;
 entity control is
 
     Port ( 
-         --        clk  : in std_logic;
-             ctrl_select     : in std_logic;  -- negate
+             ctrl_select     : in std_logic; 
 
-             syllable        : in std_logic_vector(31 downto 0);
+             syllable         : in std_logic_vector(31 downto 0);
              operand_src      : in std_logic_vector(63 downto 0);
              operand_dst      : in std_logic_vector(63 downto 0);
              immediate        : in std_logic_vector(31 downto 0);
-             gr_add_dst       : in std_logic_vector(3 downto 0); -- destination register
+             offset           : in std_logic_vector(15 downto 0);
 
-             gr_add_w         : out std_logic_vector(3 downto 0);
-             w_e_gr           : out std_logic;
-             result_gr        : out std_logic_vector(63 downto 0)
+             PC_addr          : out std_logic_vector(15 downto 0); --address to add to PC
+             PC_add           : out std_logic;
+             PC_stop          : out std_logic;
+             PC_load          : out std_logic;
 
-         );
+);
 
 end control;
 
 architecture Behavioral of control is
 
+    signal opc : std_logic_vector(7 downto 0);
+
 begin
 
-    process(ctrl_select_n, branch_reg_cont, syllable)
-  --        process(clk)
+    opc <= syllable(7 downto 0);
+
+    process(ctrl_select, opc, immediate operand_src, operand_dst, gr_add_dst)
+
     begin
 
-        if (ctrl_select_n = '1') then
+        if (ctrl_select = '0') then
 
-            branch_add <= (others => '0');
-            branch_valid <= '0';
+            PC_addr <= (others => '0');
+            PC_load <= '0';
+            PC_add <= '0';
+            PC_stop <= '0';
 
         else 
-      --        rising_edge(clk) then
 
+            case opc is
 
-            if (std_match(syllable(31 downto 26),BRT_OPC)) then
+                when JA_OPC =>
 
-                branch_add <= syllable(7 downto 0);
-                branch_valid <= branch_reg_cont;
+                    PC_addr <= offset;
+                    PC_add <= '1';
 
-            elsif (std_match(syllable(31 downto 26), BRF_OPC)) then
+                when JEQI_OPC =>
 
-                branch_add <= syllable(7 downto 0);
-                branch_valid <= not(branch_reg_cont);
+                    if (operand_dst(31 downto 0) = immediate) then
 
-            elsif (std_match(syllable(31 downto 26), JMP_OPC)) then
+                        PC_addr <= offset;
+                        PC_add <= '1';
 
-                branch_add <= syllable( 7 downto 0);
-                branch_valid <= '1';
+                    end if
 
-            else 
+                when JEQ_OPC =>
 
-                branch_valid <= '0';
-                branch_add <= (others => '0');
+                    if (operand_dst = operand_src) then
 
-            end if;
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JGTI_OPC =>
+
+                    if (operand_dst(31 downto 0) > immediate) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JGT_OPC =>
+
+                    if (operand_dst > operand_src) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JGEI_OPC =>
+
+                    if (operand_dst(31 downto 0) >= immediate) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JGE_OPC =>
+
+                    if (operand_dst >= operand_src) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JLTI_OPC =>
+
+                    if (operand_dst(31 downto 0) < immediate) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JLT_OPC =>
+
+                    if (operand_dst < operand_src) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JLEI_OPC =>
+
+                    if (operand_dst(31 downto 0) <= immediate) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JLE_OPC =>
+
+                    if (operand_dst <= operand_src) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSETI_OPC =>
+
+                    if (operand_dst(31 downto 0) and immediate) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSET_OPC =>
+
+                    if (operand_dst and operand_src) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JNEI_OPC =>
+
+                    if (operand_dst(31 downto 0) /= immediate) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JNE_OPC =>
+
+                    if (operand_dst /= operand_src) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSGTIS_OPC =>
+
+                    if (signed(operand_dst(31 downto 0)) > signed(immediate)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSGTS_OPC =>
+
+                    if (signed(operand_dst) > signed(operand_src)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSGEIS_OPC =>
+
+                    if (signed(operand_dst(31 downto 0)) >= signed(immediate)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSGES_OPC =>
+
+                    if (signed(operand_dst) >= signed(operand_src)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSLTIS_OPC =>
+
+                    if (signed(operand_dst(31 downto 0)) < signed(immediate)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSLTS_OPC =>
+
+                    if (signed(operand_dst) < signed(operand_src)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSLEIS_OPC =>
+
+                    if (signed(operand_dst(31 downto 0)) <= signed(immediate)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when JSLES_OPC =>
+
+                    if (signed(operand_dst) <= signed(operand_src)) then
+
+                        PC_addr <= offset;
+                        PC_add <= '1';
+
+                    end if
+
+                when CALL_OPC =>
+
+                    PC_addr <= immediate(15 downto 0);
+                    PC_load <= '1';
+
+                when EXIT_OPC =>
+
+                    PC_addr <= immediate(15 downto 0);
+                    PC_stop <= '1';
+
+                end if
+
+            end case;
+
 
         end if;
 
