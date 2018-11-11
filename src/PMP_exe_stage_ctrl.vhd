99612b79f52dc9074dab9f5ff01dbd45368419a6
marcux95 <marco.spaziani.brunella@uniroma2.it>
2018-11-11 11:35:59 +0100

    Completed exe stage

--- src/PMP_exe_stage_ctrl.vhd
+++ src/PMP_exe_stage_ctrl.vhd
@@ -6,22 +6,34 @@ entity exe_stage_complete is
 
     Port (
 
-             reset                     : in std_logic;
-             syllable                  : in std_logic_vector(63 downto 0);
+             reset             : in std_logic;
+             syllable          : in std_logic_vector(63 downto 0);
 
-             exe_operand_src           : in std_logic_vector(63 downto 0); -- SRC reg content
-             exe_operand_dst           : in std_logic_vector(63 downto 0); -- DST reg content
-             exe_immediate             : in std_logic_vector(31 downto 0); -- immediate in the instruction
-             exe_opc                   : in std_logic_vector(1 downto 0);  -- execution stage opc 00=alu64, 01=alu32, 10= mem, 11= branch
-             exe_dst_addr              : in std_logic_vector(3 downto 0);  -- exe stage destination register for writeback 
-             exe_offset                : in std_logic_vector(15 downto 0); -- offset inside instruction
-             exe_data_from_mem         : in std_logic_vector(63 downto 0); -- data from mem prefetched by instruction decode
+             exe_operand_src   : in std_logic_vector(63 downto 0); -- SRC reg content
+             exe_operand_dst   : in std_logic_vector(63 downto 0); -- DST reg content
+             exe_immediate     : in std_logic_vector(31 downto 0); -- immediate in the instruction
+             exe_opc           : in std_logic_vector(1 downto 0);  -- execution stage opc 00=alu64, 01=alu32, 10= mem, 11= branch
+             exe_dst_addr      : in std_logic_vector(3 downto 0);  -- exe stage destination register for writeback 
+             exe_offset        : in std_logic_vector(15 downto 0); -- offset inside instruction
+             exe_data_from_mem : in std_logic_vector(63 downto 0); -- data from mem prefetched by instruction decode
 
-    -- GPR REGISTERS INTERFACE
-             exe_result                : out std_logic_vector(63 downto 0);  -- result from EXE stage for lane forwarding and writeback
-             w_e_wb                    : out std_logic;
-             wb_reg_add                : out std_logic_vector(3 downto 0)   -- current register address in writeback from exe stage  -> for lane forwrding   
-    -- 
+             -- GPR REGISTERS INTERFACE
+             exe_result        : out std_logic_vector(63 downto 0);  -- result from EXE stage for lane forwarding and writeback
+             w_e_wb            : out std_logic;
+             wb_reg_add        : out std_logic_vector(3 downto 0);   -- current register address in writeback from exe stage   
+
+             -- MEMORY INTERFACE
+             mem_data_in      : in std_logic_vector (63 downto 0);
+             mem_data_out     : out std_logic_vector (63 downto 0);
+             mem_read_addr    : out std_logic_vector (63 downto 0);
+             mem_wrt_addr     : out std_logic_vector (63 downto 0);
+             mem_wrt_en       : out std_logic;
+
+             -- PROGRAM COUNTER INTERFACE
+             pc_addr          : out std_logic_vector(15 downto 0); -- branch address from control unit
+             pc_add           : out std_logic;    
+             pc_stop          : out std_logic;
+             pc_load          : out std_logic
 
          );
 
@@ -29,55 +41,109 @@ end exe_stage_complete;
 
 architecture Behavioral of exe_stage_complete is
 
+    -- ALU32 SIGNALS
     signal alu32_gr_result_s : std_logic_vector(63 downto 0);
     signal alu32_gr_add_s    : std_logic_vector(3 downto 0);
     signal alu32_gr_w_e_s    : std_logic;
-    
+
+    -- ALU64 SIGNALS
     signal alu64_gr_result_s : std_logic_vector(63 downto 0);
     signal alu64_gr_add_s    : std_logic_vector(3 downto 0);
     signal alu64_gr_w_e_s    : std_logic;
 
-    signal mem_select_s          : std_logic;
-    signal alu_32_select_s       : std_logic;
-    signal alu_64_select_s       : std_logic;
-    signal ctrl_select_s         : std_logic;
+    -- MEM SIGNALS
+    signal mem_data_out_s     : std_logic_vector (63 downto 0);
+    signal mem_read_addr_s    : std_logic_vector (63 downto 0);
+    signal mem_wrt_addr_s     : std_logic_vector (63 downto 0);
+    signal mem_wrt_en_s       : std_logic;
+    signal mem_gr_result_s    : std_logic_vector(63 downto 0);
+    signal mem_gr_add_s       : std_logic_vector(3 downto 0);
+    signal mem_gr_w_e_s       : std_logic;
 
---signal alu_immediate_s : std_logic_vector(31 downto 0);
+    --CTRL SIGNALS
+    signal ctrl_pc_address_s  : std_logic_vector(15 downto 0);
+    signal ctrl_pc_add_s      : std_logic;
+    signal ctrl_pc_stop_s     : std_logic;
+    signal ctrl_pc_load_s     : std_logic;
+
+    -- SELECT SIGNALS
+    signal mem_select_s       : std_logic;
+    signal alu_32_select_s    : std_logic;
+    signal alu_64_select_s    : std_logic;
+    signal ctrl_select_s      : std_logic;
 
 begin
 
     ALU32: entity work.alu32 port map 
     (
 
-    alu32_select => alu_32_select_s,
-    syllable => syllable,
-    operand_src => exe_operand_src,    
-    operand_dst => exe_operand_dst,
-    immediate => exe_immediate,
-    gr_add_dst =>exe_dst_addr,
-    
-    gr_add_w => alu32_gr_add_s,
-    w_e_gr => alu32_gr_w_e_s,
-    result_gr => alu32_gr_result_s
+        alu32_select => alu_32_select_s,
+        syllable => syllable,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        gr_add_dst =>exe_dst_addr,
+
+        gr_add_w => alu32_gr_add_s,
+        w_e_gr => alu32_gr_w_e_s,
+        result_gr => alu32_gr_result_s
 
     );
-    
+
     ALU64: entity work.alu64 port map 
     (
 
-    alu32_select => alu_32_select_s,
-    syllable => syllable,
-    operand_src => exe_operand_src,    
-    operand_dst => exe_operand_dst,
-    immediate => exe_immediate,
-    gr_add_dst =>exe_dst_addr,
-    
-    gr_add_w => alu32_gr_add_s,
-    w_e_gr => alu32_gr_w_e_s,
-    result_gr => alu32_gr_result_s
+        alu64_select => alu_64_select_s,
+        syllable => syllable,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        gr_add_dst =>exe_dst_addr,
+
+        gr_add_w => alu64_gr_add_s,
+        w_e_gr => alu64_gr_w_e_s,
+        result_gr => alu64_gr_result_s
 
     );
 
+    MEM: entity work.memory_unit port map 
+    (
+
+        mem_select => mem_select_s,
+        syllable => syllable,
+        reset => reset,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        offset => exe_offset,
+        gr_add_dst =>exe_dst_addr,
+
+        mem_data_in => mem_data_in,
+        mem_data_out => mem_data_out_s,
+        mem_read_addr => mem_read_addr_s,
+        mem_wrt_en => mem_wrt_en_s,
+
+        gr_add_w => mem_gr_add_s,
+        w_e_gr => mem_gr_w_e_s,
+        result_gr => mem_gr_result_s
+
+    );
+
+    CTRL: entity work.control port map 
+    (
+
+        ctrl_select => ctrl_select_s,
+        syllable => syllable,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        offset => exe_offset,
+
+        PC_addr => ctrl_pc_address_s,
+        PC_add => ctrl_pc_add_s,
+        PC_stop => ctrl_pc_stop_s,
+        PC_load => ctrl_pc_load_s
+    );
 
     -- SELECTING UNIT
     alu_64_select_s <= '1' when exe_opc = "00" else
@@ -91,5 +157,48 @@ begin
 
     ctrl_select_s <= '1' when exe_opc = "11" else
                      '0';
+
+    -- MUXING GPR ACCESS
+
+    exe_result <= alu64_gr_result_s when exe_opc = "00" else
+                  alu32_gr_result_s when exe_opc = "01" else
+                  mem_gr_result_s when exe_opc = "10" else
+                  (others => '0');
+
+    w_e_wb <= alu64_gr_w_e_s when exe_opc = "00" else
+              alu32_gr_w_e_s when exe_opc = "01" else
+              mem_gr_w_e_s when exe_opc = "10" else
+              '0';
+
+    wb_reg_add <= alu64_gr_add_s when exe_opc = "00" else
+                  alu32_gr_add_s when exe_opc = "01" else
+                  mem_gr_add_s when exe_opc = "10" else
+                  (others => '0');
+
+    -- MUXING MEM ACCESS
+
+    mem_data_out <= mem_data_out_s when exe_opc = "10" else
+                    (others => '0');
+    mem_read_addr <= mem_read_addr_s when exe_opc = "10" else
+                     (others = '0');
+    mem_wrt_addr <= mem_wrt_addr_s when exe_opc = "10" else
+                    (others => '0');
+    mem_wrt_en <= mem_wrt_en_s when exe_opc = "10" else
+                  '0';
+
+    -- MUXING PC ACCESS
+
+    PC_addr <= ctrl_pc_address_s when exe_opc = "11" else
+               (others => '0');
+    
+    PC_add <= ctrl_pc_add_s when exe_opc = "11" else
+               (others => '0');
+    
+    PC_stop <= ctrl_pc_stop_s when exe_opc = "11" else
+               (others => '0');
+    
+    PC_load <= ctrl_pc_load_s when exe_opc = "11" else
+               (others => '0');
+
 
 end Behavioral;
--- src/PMP_exe_stage_noctrl.vhd
+++ src/PMP_exe_stage_noctrl.vhd
@@ -1,125 +1,160 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity exe_stage_1_3 is
+-- WITH CONTROL UNIT
+entity exe_stage_complete is
 
-  Port (
-         reset          : in std_logic;
-         exe_opc        : in std_logic_vector(1 downto 0);
+    Port (
 
-         syllable        : in std_logic_vector(31 downto 0);
-         alu_oper_0      : in std_logic_vector(31 downto 0);
-         alu_oper_1      : in std_logic_vector(31 downto 0);
-         alu_imm         : in std_logic_vector(10 downto 0);
-         alu_gr_dest_add : in std_logic_vector(4 downto 0);
+             reset             : in std_logic;
+             syllable          : in std_logic_vector(63 downto 0);
 
-         mem_dest_reg   : in std_logic_vector(4 downto 0);
-         mem_store_data : in std_logic_vector(31 downto 0);
-         mem_l_s        : in std_logic;
-         mem_add_dec    : in std_logic_vector(31 downto 0);
+             exe_operand_src   : in std_logic_vector(63 downto 0); -- SRC reg content
+             exe_operand_dst   : in std_logic_vector(63 downto 0); -- DST reg content
+             exe_immediate     : in std_logic_vector(31 downto 0); -- immediate in the instruction
+             exe_opc           : in std_logic_vector(1 downto 0);  -- execution stage opc 00=alu64, 01=alu32, 10= mem, 11= branch
+             exe_dst_addr      : in std_logic_vector(3 downto 0);  -- exe stage destination register for writeback 
+             exe_offset        : in std_logic_vector(15 downto 0); -- offset inside instruction
+             exe_data_from_mem : in std_logic_vector(63 downto 0); -- data from mem prefetched by instruction decode
 
-         alu_br_dest_add : in std_logic_vector(4 downto 0);  --destination address of branch register to be written
-         jump_add        : in std_logic_vector( 7 downto 0); -- address to jump
-         alu_br_cont     : in std_logic; -- branch register content
-                                         -- Memory interface
-         mem_add_wrt     : out std_logic_vector(31 downto 0); -- used only to store
-         mem_data_out    : out std_logic_vector(31 downto 0); -- data to write in mem
-         mem_wrt_amnt    : out std_logic_vector(4 downto 0);  -- number of bits written to memory
-         mem_data_in     : in std_logic_vector(31 downto 0);  -- data from memory
-         mem_w_e         : out std_logic;                     -- memory write enable
-                                                              -- GR File interface
-         gr_add_w       : out std_logic_vector(4 downto 0);
-         gr_data_w      : out std_logic_vector(31 downto 0);
-         gr_w_e         : out std_logic;
-        -- BR File Interface
-         br_add_w       : out std_logic_vector( 4 downto 0);
-         br_data_w      : out std_logic;
-         br_w_e         : out std_logic
+             -- GPR REGISTERS INTERFACE
+             exe_result        : out std_logic_vector(63 downto 0);  -- result from EXE stage for lane forwarding and writeback
+             w_e_wb            : out std_logic;
+             wb_reg_add        : out std_logic_vector(3 downto 0);   -- current register address in writeback from exe stage   
 
-       );
+             -- MEMORY INTERFACE
+             mem_data_in      : in std_logic_vector (63 downto 0);
+             mem_data_out     : out std_logic_vector (63 downto 0);
+             mem_read_addr    : out std_logic_vector (63 downto 0);
+             mem_wrt_addr     : out std_logic_vector (63 downto 0);
+             mem_wrt_en       : out std_logic
 
-end exe_stage_1_3;
+         );
 
-architecture Behavioral of exe_stage_1_3 is
+end exe_stage_complete;
 
-  signal alu_gr_result_s : std_logic_vector(31 downto 0);
-  signal alu_br_result_s : std_logic;
-  signal alu_gr_add_s    : std_logic_vector(4 downto 0);
-  signal alu_br_add_s    : std_logic_vector(4 downto 0);
-  signal alu_gr_w_e_s    : std_logic;
-  signal alu_br_w_e_s    : std_logic;
+architecture Behavioral of exe_stage_complete is
 
-  signal mem_gr_result_s : std_logic_vector(31 downto 0);
-  signal mem_gr_add_s    : std_logic_vector(4 downto 0);
-  signal mem_gr_w_e_s    : std_logic;
+    -- ALU32 SIGNALS
+    signal alu32_gr_result_s : std_logic_vector(63 downto 0);
+    signal alu32_gr_add_s    : std_logic_vector(3 downto 0);
+    signal alu32_gr_w_e_s    : std_logic;
 
-  signal mem_select_n_s       : std_logic;
-  signal alu_select_n_s       : std_logic;
-  signal alu_immediate_s : std_logic_vector(31 downto 0);
+    -- ALU64 SIGNALS
+    signal alu64_gr_result_s : std_logic_vector(63 downto 0);
+    signal alu64_gr_add_s    : std_logic_vector(3 downto 0);
+    signal alu64_gr_w_e_s    : std_logic;
+
+    -- MEM SIGNALS
+    signal mem_data_out_s     : std_logic_vector (63 downto 0);
+    signal mem_read_addr_s    : std_logic_vector (63 downto 0);
+    signal mem_wrt_addr_s     : std_logic_vector (63 downto 0);
+    signal mem_wrt_en_s       : std_logic;
+    signal mem_gr_result_s    : std_logic_vector(63 downto 0);
+    signal mem_gr_add_s       : std_logic_vector(3 downto 0);
+    signal mem_gr_w_e_s       : std_logic;
+
+    -- SELECT SIGNALS
+    signal mem_select_s       : std_logic;
+    signal alu_32_select_s    : std_logic;
+    signal alu_64_select_s    : std_logic;
 
 begin
 
-  alu_immediate_s <= x"00000" & '0' & alu_imm;
+    ALU32: entity work.alu32 port map 
+    (
 
-  ALU: entity work.alu_pmp port map (
-  alu_select_n_s,
+        alu32_select => alu_32_select_s,
+        syllable => syllable,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        gr_add_dst =>exe_dst_addr,
 
-  syllable,
-  alu_oper_0,
-  alu_oper_1,
-  alu_immediate_s,
-  alu_gr_dest_add,
-  alu_br_dest_add,
+        gr_add_w => alu32_gr_add_s,
+        w_e_gr => alu32_gr_w_e_s,
+        result_gr => alu32_gr_result_s
 
-  alu_gr_add_s,
-  alu_br_add_s,
-  alu_gr_w_e_s,
-  alu_br_w_e_s,
-  alu_gr_result_s,
-  alu_br_result_s
+    );
 
-);
+    ALU64: entity work.alu64 port map 
+    (
 
-MEM: entity work.mem_pmp port map (
-mem_select_n_s,
+        alu64_select => alu_64_select_s,
+        syllable => syllable,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        gr_add_dst =>exe_dst_addr,
 
-syllable,
-mem_dest_reg,
-mem_l_s,
-mem_store_data,
-mem_data_in,
-mem_add_dec,
-mem_add_wrt,
-mem_data_out,
-mem_wrt_amnt,
-mem_w_e,
-mem_gr_result_s,
-mem_gr_add_s,
-mem_gr_w_e_s
+        gr_add_w => alu64_gr_add_s,
+        w_e_gr => alu64_gr_w_e_s,
+        result_gr => alu64_gr_result_s
 
-                                   );
+    );
 
-                                   gr_add_w  <= alu_gr_add_s when exe_opc = "00" else
-                                                mem_gr_add_s when exe_opc = "10" else
-                                                (others => '0');
+    MEM: entity work.memory_unit port map 
+    (
 
-                                   gr_data_w <= alu_gr_result_s when exe_opc = "00" else
-                                                mem_gr_result_s when exe_opc = "10" else
-                                                (others => '0');
+        mem_select => mem_select_s,
+        syllable => syllable,
+        reset => reset,
+        operand_src => exe_operand_src,    
+        operand_dst => exe_operand_dst,
+        immediate => exe_immediate,
+        offset => exe_offset,
+        gr_add_dst =>exe_dst_addr,
 
-                                   gr_w_e <= alu_gr_w_e_s  when exe_opc = "00" else
-                                             mem_gr_w_e_s when exe_opc = "10" else
-                                             '0';
+        mem_data_in => mem_data_in,
+        mem_data_out => mem_data_out_s,
+        mem_read_addr => mem_read_addr_s,
+        mem_wrt_en => mem_wrt_en_s,
 
-                                   alu_select_n_s <= '0' when exe_opc = "00" else
-                                                     '1';
+        gr_add_w => mem_gr_add_s,
+        w_e_gr => mem_gr_w_e_s,
+        result_gr => mem_gr_result_s
 
-                                   mem_select_n_s <= '0' when exe_opc = "10" else
-                                                     '1';
+    );
 
 
-                                   br_add_w <= alu_br_add_s;
-                                   br_data_w <= alu_br_result_s;
-                                   br_w_e <= alu_br_w_e_s;
+    -- SELECTING UNIT
+    alu_64_select_s <= '1' when exe_opc = "00" else
+                       '0';
 
-                                 end Behavioral;     
+    alu_32_select_s <= '1' when exe_opc = "01" else
+                       '0';
+
+    mem_select_s <= '1' when exe_opc = "10" else
+                    '0';
+
+
+    -- MUXING GPR ACCESS
+
+    exe_result <= alu64_gr_result_s when exe_opc = "00" else
+                  alu32_gr_result_s when exe_opc = "01" else
+                  mem_gr_result_s when exe_opc = "10" else
+                  (others => '0');
+
+    w_e_wb <= alu64_gr_w_e_s when exe_opc = "00" else
+              alu32_gr_w_e_s when exe_opc = "01" else
+              mem_gr_w_e_s when exe_opc = "10" else
+              '0';
+
+    wb_reg_add <= alu64_gr_add_s when exe_opc = "00" else
+                  alu32_gr_add_s when exe_opc = "01" else
+                  mem_gr_add_s when exe_opc = "10" else
+                  (others => '0');
+
+    -- MUXING MEM ACCESS
+
+    mem_data_out <= mem_data_out_s when exe_opc = "10" else
+                    (others => '0');
+    mem_read_addr <= mem_read_addr_s when exe_opc = "10" else
+                     (others = '0');
+    mem_wrt_addr <= mem_wrt_addr_s when exe_opc = "10" else
+                    (others => '0');
+    mem_wrt_en <= mem_wrt_en_s when exe_opc = "10" else
+                  '0';
+
+
+end Behavioral;
